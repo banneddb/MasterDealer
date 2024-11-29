7126b026a7d3aa921cf81e6028516a11f0a100e5
MisaelManz <tut27151@temple.edu>
2024-11-29 09:25:35 -0500

    Finished the code for the bot response. Now it finally answers our messages...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("hola mi loco")
 from typing import Final
 import os
 from dotenv import load_dotenv
@@ -22,6 +23,31 @@ async def send_message(message: Message, user_message:str) -> None:
         user_message = user_message[1:]
     try:
         response: str = get_response(user_message)
-        await message.author.send(response) if is_private else message.channel.send(response)
+        await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+# STEP 3: HANDLING THE STARTUP FOR OUR BOT
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+# STEP 4: HANDLING INCOMING MESSAGES
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+# STEP 5: MAIN ENTRY POINT
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
