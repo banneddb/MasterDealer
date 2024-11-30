809d212085994a67aea3819891e1ef862b30c91c
MisaelManz <tut27151@temple.edu>
2024-11-30 13:00:52 -0500

    hola

--- bot_startup.py
+++ bot_startup.py
@@ -4,6 +4,16 @@ import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from bot_response import get_response
+# Import the Canvas class
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = "https://example.com"
+# Canvas API key
+API_KEY = "p@$$w0rd"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
 
 # Loading the token into the program
 load_dotenv()
